ceed2082f474405402075d905c8249d896fe8c8e
Lucas Franqueira <lucas.f@gec.inatel.br>
2024-08-21 11:30:34 -0300

    :sparkles: Working with templates

--- base/views.py
+++ base/views.py
@@ -1,3 +1,27 @@
 from django.shortcuts import render
 
-# Create your views here.
+rooms = [
+    {'id': 1, 'name': 'Room 1'},
+    {'id': 2, 'name': 'Room 2'},
+    {'id': 3, 'name': 'Room 3'},
+]
+
+
+def home(request):
+    context = {
+        'rooms': rooms
+    }
+    return render(request, 'base/home.html', context)
+
+
+def room(request, pk):
+    room = None
+    for i in rooms:
+        if i['id'] == int(pk):
+            room = i
+            break
+
+    context = {
+        'room': room
+    }
+    return render(request, 'base/room.html', context)

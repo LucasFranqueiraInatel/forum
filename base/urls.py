c2cf24ebb33123ce26e744743cbb8f046f451d25
Lucas Franqueira <lucas.f@gec.inatel.br>
2024-08-26 15:32:35 -0300

    :sparkles: Adjusting mobile responsiveness and configuring api

--- base/urls.py
+++ base/urls.py
@@ -12,9 +12,11 @@ urlpatterns = [
     path('update-room/<str:pk>/', views.updateRoom, name='update-room'),
     path('delete-room/<str:pk>/', views.deleteRoom, name='delete-room'),
 
+    path('activity/', views.activityPage, name='activity'),
+    path('topics/', views.topicsPage, name='topics'),
+
     path('profile/<str:pk>/', views.userProfile, name='user-profile'),
     path('update-user/<str:pk>/', views.updateUser, name='update-user'),
 
     path('delete-message/<str:pk>/', views.deleteMessage, name='delete-message'),
 ]
-
--- base/views.py
+++ base/views.py
@@ -206,3 +206,17 @@ def updateUser(request, pk):
         'form': form
     }
     return render(request, 'base/update_user.html', context)
+
+
+def activityPage(request):
+    messages = Message.objects.filter(Q(user=request.user))
+    context = {
+        'messages': messages
+    }
+    return render(request, 'base/activity.html', context)
+
+
+def topicsPage(request):
+    q = request.GET.get('q') if request.GET.get('q') != None else ''
+    topics = Topic.objects.filter(name__icontains=q)
+    return render(request, 'base/topics.html', {'topics': topics})
